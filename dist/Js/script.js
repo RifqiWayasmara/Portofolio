3620dc0a4f8f36ceb25003b2e756da793def0286
RifqiWayasmara <RifqiWayasmara87@gmail.com>
2023-01-16 14:17:21 +0700

    finished all

--- dist/Js/script.js
+++ dist/Js/script.js
@@ -2,17 +2,19 @@
 window.onscroll = function(){
     const header = document.querySelector('header');
     const FixedNav = header.offsetTop;
-    console.log(FixedNav)
+    const toTop = document.querySelector('#to-top');
+
 
     if(window.pageYOffset > FixedNav){
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     }else{
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 };
-
-
-
 
 //Hamburger
 const Hamburger = document.querySelector('#hamburger');
@@ -20,4 +22,12 @@ const navMenu   =document.querySelector('#nav-menu');
 Hamburger.addEventListener('click', function() {
     Hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
+});
+
+//Klik Diluar Hamburger
+window.addEventListener('click', function (e) {
+    if (e.target != Hamburger && e.target !=nav.navMenu) {
+        Hamburger.classList.remove("Hamburger-active");
+        navMenu.classList.add('hidden');
+    }   
 });
